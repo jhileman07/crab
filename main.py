addb5ad1a9f09d5a556bfa69185a6c7e337f031a
Andrew Li <andrewli@u.northwestern.edu>
2026-01-23 00:39:09 -0600

    default to HIGH verbosity

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import crab.process.liveness as liveness
 from crab.runner.stdout_runner import StdoutRunner, Verbosity
 
 if __name__ == "__main__":
-    runner = StdoutRunner("./tests/liveness", argc=1, verbosity=Verbosity.NOT)
+    runner = StdoutRunner("./tests/liveness", argc=1, verbosity=Verbosity.HIGH)
     runner.cd("../cs322-compiler/L2")
     runner.with_command(lambda f: f"./bin/L2 -l {f}")
     runner.with_output(lambda f: f"{f}.out")
