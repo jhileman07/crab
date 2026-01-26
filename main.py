7b0df20f7960c7d125b63e3791e01be3e9ebb17b
Andrew Li <andrewli@u.northwestern.edu>
2026-01-25 20:25:48 -0600

    add spill tests

--- main.py
+++ main.py
@@ -18,3 +18,10 @@ if __name__ == "__main__":
     runner.with_args("*.L2f")
     runner.bind_postprocessor(interference.sort_lines)
     runner.run()
+    
+    runner = StdoutRunner("./tests/spill", argc=1, verbosity=Verbosity.FIRST_FAIL)
+    runner.cd("../cs322-compiler/L2")
+    runner.with_command(lambda f: f"./bin/L2 -s {f}")
+    runner.with_output(lambda f: f"{f}.out")
+    runner.with_args("*.L2f")
+    runner.run()
